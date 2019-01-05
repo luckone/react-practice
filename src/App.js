94dba55a8917fc1c3e8ea49bed9abb2d516bc6d8
Valera Milko <valeramilko@iMac-Ivan.local>
2019-01-05 15:39:19 +0200

    top of the week UI added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import SearchComponent from './components/SearchComponent';
 import TabsComponent from './components/TabsComponent';
 import DrawerPlayerComponent from './components/DrawerPlayerComponent';
 import { connect } from "react-redux";
+import AlbumImg from "./assets/pic.jpeg";
+import SongImg from "./assets/jcole.jpg";
+import ArtistImg from "./assets/lana.jpeg";
 
 class App extends Component {
     render() {
@@ -18,7 +21,47 @@ class App extends Component {
                 <PlayerComponent/>
                 <TabsComponent/>
                 <DrawerPlayerComponent/>
-                <LoadingComponent/>
+                  <main className="page">
+                    <div className="top">
+                        <div className="top-card">
+                            <div className="top-card__wrapper">
+                                <img src={AlbumImg} alt="" className="top-card__img"/>
+                                <div className="top-card__info">
+                                    <p className="top-card__info--label">TOP ALBUM OF THE WEEK</p>
+                                    <div className="centered">
+                                        <p className="top-card__info--name">Bank account</p>
+                                        <p className="top-card__info--descr">Top Album</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="top-card">
+                            <div className="top-card__wrapper">
+                                <img src={SongImg} alt="" className="top-card__img"/>
+                                <div className="top-card__info">
+                                    <p className="top-card__info--label">TOP SONG OF THE WEEK</p>
+                                    <div className="centered">
+                                        <p className="top-card__info--name">No role modelz</p>
+                                        <p className="top-card__info--descr">Top Song</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="top-card">
+                            <div className="top-card__wrapper">
+                                <img src={ArtistImg} alt="" className="top-card__img"/>
+                                <div className="top-card__info">
+                                    <p className="top-card__info--label">TOP ARTIST OF THE WEEK</p>
+                                    <div className="centered">
+                                        <p className="top-card__info--name">Lana Del Ray</p>
+                                        <p className="top-card__info--descr">Top Artist</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </main>
+                {/*<LoadingComponent/>*/}
             </div>
           </div>
     );
